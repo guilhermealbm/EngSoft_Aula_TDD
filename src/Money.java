30073230b41c2564b9bf367c78255d2b0f931820
guilhermealbm <gui.miranda.13@gmail.com>
2020-10-01 10:06:54 -0300

    commit #4

--- src/Money.java
+++ src/Money.java
@@ -1,16 +1,26 @@
 public abstract class Money {
 
     protected int amount;
+    private String currency;
 
     static Dollar dollar(int amount)  {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Franc franc(int amount)  {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
+    }
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
     }
 
     abstract Money times(int multiplier);
+
+    String currency() {
+        return currency;
+    }
 
     public boolean equals(Object object)  {
         Money money = (Money) object;
